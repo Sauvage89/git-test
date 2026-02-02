d9744dbe7901e6b2ceb0b84e1b4c48c71d2c0c50
lennylemeilleur <lennylegoat@gmail.com>
2026-02-02 07:28:41 +0100

    feat(main.c): ajout d'une fonctionnalité swap dans le fichier main.c

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #incluce <stdio.h>
 
+void	ft_swap(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
+
 int	main()
 {
-	printf("hello world !");
+	int	var_a;
+	int	var_b;
+
+	var_a = 5;
+	var_b = 10;
+
+	printf("var a : %i\n", var_a);
+	printf("var b : %i\n", var_b);
+	ft_swap(&var_a, &var_b);
+	printf("SWAP DES VALEURS\n");
+	printf("var a : %i\n", var_a);
+	printf("var b : %i\n", var_b);
 
 	return (0);
 }
